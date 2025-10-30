efd8d39d5b20269d07d566ee4018a512ac76fabc
mobinJS <mobyn3223>
2025-10-30 23:53:25 +0330

    updatedB

--- src/assets/Components/BrandsSupplements/BrandsSupplements.jsx
+++ src/assets/Components/BrandsSupplements/BrandsSupplements.jsx
@@ -4,20 +4,45 @@ import BrandsItem from "../ForBarndsProducts/BrandsItem/BrandsItem.jsx";
 import { allBrandLogo } from "../../../Data.js";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
-import "./SliderStyle.css";
 
 export default function BrandsSupplements() {
   const [sliderRef] = useKeenSlider({
+    rtl: true,
     slides: {
-      perView: 2,
+      perView: 5,
       spacing: 15,
     },
+      breakpoints: {
+    "(max-width: 1200px)": {
+      slides: {
+        perView: 4,
+        spacing: 10,
+      },
+    },
+    "(max-width: 992px)": {
+      slides: {
+        perView: 3,
+        spacing: 8,
+      },
+    },
+    "(max-width: 768px)": {
+      slides: {
+        perView: 2,
+        spacing: 6,
+      },
+    },
+    "(max-width: 480px)": {
+      slides: {
+        perView: 3,
+        spacing: 4,
+      },
+    }},
   });
   return (
     <div className="bg-white pt-4 pb-4 pr-3 pl-3 container-custom">
       <SectionsTitle title="برند های تحت پوشش" />
-      <div className="flex w-full overflow-x-hidden sm:grid grid-cols-2 x:grid-cols-3 xs:grid-cols-4 md:grid-cols-6  gap-2 mt-5 mb-5 items-center justify-center">
-        <div ref={sliderRef} className="keen-slider">
+      <div className="gap-2 mt-5 mb-5 items-center justify-center">
+        <div ref={sliderRef} className="keen-slider md:hidden w-full">
           {allBrandLogo.map((logo) => (
             <BrandsItem key={logo.id} {...logo} />
           ))}
--- src/assets/Components/ForBarndsProducts/BrandsItem/BrandsItem.jsx
+++ src/assets/Components/ForBarndsProducts/BrandsItem/BrandsItem.jsx
@@ -7,9 +7,9 @@ export default function BrandsItem({ img, name }) {
       to={`productsBrand/${name}`}
       className="pt-3 keen-slider__slide number-slide1"
     >
-      <div className="border-l-2 border-solid border-l-slate-200 border-r-2 md:border-r-0  border-r-slate-200 max-w-36 pl-3 pr-3">
+      <div className=" border-solid  max-w-36 pl-3 pr-3">
         <img
-          className="w-42 h-[70px] hover:border-2 hover:border-sky-400 hover:border-solid object-cover opacity-60 hover:opacity-100 transition-all cursor-pointer  shadow-brands border rounded-full border-slate-200 border-solid "
+          className="w-64 h-[70px] hover:border-2 hover:border-sky-400 hover:border-solid object-cover opacity-60 hover:opacity-100 transition-all cursor-pointer  shadow-brands border rounded-full border-slate-200 border-solid "
           src={img}
           alt=""
         />
